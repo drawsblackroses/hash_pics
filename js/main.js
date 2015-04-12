78fd04421c6bb0ea30fba8e994c38f9f025a9e00
lwyatt <lwyatt@innolance.com>
2015-04-11 22:08:38 -0400

    correct url for instagram api and include pagination parameters with api call

--- js/main.js
+++ js/main.js
@@ -2,26 +2,36 @@ $(document).ready(function(){
 
 
 	var defaultHash = 'dctech',
+		numberPerPage = 4,
 		instagramClientID = 'e9561311b628484a80a738312c222079',
-		instagramURL = 'https://api.instagram.com/v1/tags/search';
+		instagramURL = 'https://api.instagram.com/v1/tags/',
+		instagramURL2 = '/media/recent',
+		instagramImages = [];
 
-	function getInstagramImages(hash){
+	function getInstagramImages(hash, page){
+		var first_id = (page * numberPerPage) - 1;
+		var last_id = first_id + (numberPerPage - 1);
 		$.ajax({
-			url: instagramURL,
+			url: instagramURL + hash + instagramURL2,
 			method: 'get',
 			dataType: 'jsonp', // added to fix cors issue
 			crossDomain: true, // added to fix cors issue
 			data: {
-				q: hash,
-				client_id: instagramClientID
+				client_id: instagramClientID,
+				count: numberPerPage,
+				min_tag_id: first_id,
+				max_tag_id: last_id
 			},
 			success: function(data){
-				console.log(data);
+				instagramImages = [];
+				$.each(data.data, function(){
+					instagramImages.push(this['images']['low_resolution']['url']);
+				}); console.log(instagramImages);
 			}
 		});
 	}
 
-	getInstagramImages(defaultHash);
+	getInstagramImages(defaultHash, 1);
 
 });
 
