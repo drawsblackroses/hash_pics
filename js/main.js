6c9a367a97309247834f66de1f79a9258d7731d8
lwyatt <lwyatt@innolance.com>
2015-04-11 23:16:49 -0400

    add loading icon and load images onto the page

--- js/main.js
+++ js/main.js
@@ -3,14 +3,20 @@ $(document).ready(function(){
 
 	var defaultHash = 'dctech',
 		numberPerPage = 4,
+		addedImageClass = 'image-found',
+		$imageBlock = $('.no-images-found').first()
+								.clone().attr('id','')
+								.removeClass('no-images-found')
+								.addClass(addedImageClass),
+		$noInstagramImages = $('#no-instagram-images-found'),
+		$instagramContainer = $('#instagram-images'),
 		instagramClientID = 'e9561311b628484a80a738312c222079',
 		instagramURL = 'https://api.instagram.com/v1/tags/',
 		instagramURL2 = '/media/recent',
 		instagramImages = [];
 
 	function getInstagramImages(hash, page){
-		var first_id = (page * numberPerPage) - 1;
-		var last_id = first_id + (numberPerPage - 1);
+		$instagramContainer.addClass('loading');
 		$.ajax({
 			url: instagramURL + hash + instagramURL2,
 			method: 'get',
@@ -19,15 +25,37 @@ $(document).ready(function(){
 			data: {
 				client_id: instagramClientID,
 				count: numberPerPage,
-				min_tag_id: first_id,
-				max_tag_id: last_id
+				//min_tag_id: first_id, <- should be used for "previous" button
+				//max_tag_id: last_id  <- should be used for "next" button
 			},
 			success: function(data){
 				instagramImages = [];
-				$.each(data.data, function(){
-					instagramImages.push(this['images']['low_resolution']['url']);
-				}); console.log(instagramImages);
+				var imageData = data.data;
+				$instagramContainer.removeClass('loading');
+				$instagramContainer.find('.'+addedImageClass).remove();
+				if(imageData.length > 0){
+					$noInstagramImages.hide();
+					$.each(imageData, function(){
+						instagramImages.push({
+							url: this['images']['low_resolution']['url'],
+							caption: this['caption']['text']
+						});
+					});
+					addImagesToPage($instagramContainer, instagramImages);
+				} else {
+					$noInstagramImages.show();
+				}
 			}
+		});
+	}
+
+	function addImagesToPage($section, images){
+		$.each(images, function(){
+			$thisImageBlock = $imageBlock.clone();
+			$thisImageBlock.find('img')
+				.attr('src',this.url)
+				.attr('alt',this.caption);
+			$section.append($thisImageBlock);
 		});
 	}
 
